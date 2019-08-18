4836ad0e9f2af0dbe6276acf6902381699f5afc7
Luis Finke <lfinke@fusionalliance.com>
2019-08-18 00:21:53 -0500

    SoundHoleCore project

---
+++ src/test/main.cpp
@@ -0,0 +1,11 @@
+//
+//  main.cpp
+//  SoundHoleCoreTest
+//
+//  Created by Luis Finke on 8/18/19.
+//  Copyright © 2019 Luis Finke. All rights reserved.
+//
+
+int main(int argc, char* argv[]) {
+	return 0;
+}
