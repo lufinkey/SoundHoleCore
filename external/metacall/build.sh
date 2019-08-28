cdbc2846914658b5694cc98470f245f476a0d546
Luis Finke <lfinke@fusionalliance.com>
2019-08-27 23:10:04 -0400

    add metacall

--- external/metacall/build.sh
+++ external/metacall/build.sh
@@ -5,8 +5,26 @@ base_dir=$(dirname "${BASH_SOURCE[0]}")
 cd "$base_dir"
 base_dir="$PWD"
 
+# get arguments
+PLATFORM="$1"
+if [ -z "$PLATFORM" ]; then
+	>&2 echo "platform must be given as 1st argument"
+	exit 1
+fi
+
 # build core
-mkdir "core/build"
-cd "core/build"
-cmake .. -OPTION_SELF_CONTAINED=On -DOPTION_BUILD_LOADERS_C=On -DOPTION_BUILD_LOADERS_NODE=On -DOPTION_BUILD_SERIALS=On || exit $?
+mkdir -p "core/build/$PLATFORM"
+cd "core/build/$PLATFORM"
+mkdir -p "prefix"
+if [ -f ".buildSuccess" ]; then
+	echo "platform $platform already built. skipping..."
+	exit 0
+fi
+cmake ../../ \
+	-DCMAKE_INSTALL_PREFIX="$PWD/prefix" \
+	-DOPTION_SELF_CONTAINED=On -DBUILD_SHARED_LIBS=Off \
+	-DOPTION_BUILD_LOADERS_C=On -DOPTION_BUILD_LOADERS_NODE=On -DOPTION_BUILD_SERIALS=On \
+	|| exit $?
 make || exit $?
+make install || exit $?
+touch .buildSuccess
