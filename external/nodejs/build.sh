4439367d518ae5b82cfdadd809db11cc3af0325d
Luis Finke <lfinke@fusionalliance.com>
2019-09-07 17:33:06 -0400

    better build scripts for nodejs

--- external/nodejs/build.sh
+++ external/nodejs/build.sh
@@ -5,13 +5,6 @@ base_dir=$(dirname "${BASH_SOURCE[0]}")
 cd "$base_dir"
 base_dir="$PWD"
 
-# clone nodejs
-mkdir -p "build" || exit $?
-cd "build"
-if [ ! -d "node/.git" ] && [ ! -f "node/.git" ]; then
-	git clone --recursive "https://github.com/nodejs/node" || exit $?
-fi
-
 # get arguments
 build_type="$1"
 if [ -z "$build_type" ]; then
@@ -22,19 +15,19 @@ if [ "$build_type" == "Debug" ]; then
 	configure_args+=("--debug")
 fi
 
+# fetch nodejs
+./fetch_node.sh || exit $?
+
+# build header files
+./build_headers.sh || exit $?
+
 # build nodejs
-cd "$base_dir/build/node"
+cd "$base_dir/build/node" || exit $?
 ./configure --fully-static --enable-static "${configure_args[@]}" || exit $?
 make -j4 || exit $?
 
 # copy output files
-cd "$base_dir/build"
+cd "$base_dir/build" || exit $?
 mkdir -p "$build_type" || exit $?
-cd "$base_dir/build/node/out/$build_type"
+cd "$base_dir/build/node/out/$build_type" || exit $?
 cp *.a "$base_dir/build/$build_type" || exit $?
-
-# link include files
-cd "$base_dir/build"
-mkdir -p "include"
-cd "include"
-ln -s "../node/src" "nodejs"
